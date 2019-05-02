ab31036bc84a84ff238fede262ede72b953a8930
aefritz <austin.fritz33@gmail.com>
2019-05-02 09:50:49 -0400

    fixed file writing

--- test_file_write.py
+++ test_file_write.py
@@ -5,6 +5,7 @@ def create_file():
     first_line = input("Enter the first line")
     with open(filename, "w+") as file_object:
         file_object.write(first_line)
+    main()
 
 def read_file(filename):
     with open(filename) as file_object:
@@ -18,26 +19,45 @@ def read_file(filename):
         main()
 
 def edit_file(filename):
-    print("To be constructed")
-
+    editloop = True
+    print("Press enter to write a new line or Q to quit\n")
+    while editloop:
+        newline = input("")
+        if (newline == 'Q'):
+            break
+        else:
+            newline = '\n' + newline
+            with open(filename, "a") as file_object:
+                file_object.write(newline)
+    main()
 
 def main():
-    print("Here are all the files in this directory")
+    print("\nHere are all the files in this directory\n")
     files = os.listdir()
     for file in files:
         print(str(files.index(file)) + ") " + file)
-    selection = input("Select a file\nEnter L to change directories\nEnter N to create a new file\nEnter Q to quit "))
-    if (type(int(selection)) is int):
-        if (int(selection) >= 0) and (int(selection) <= (len(files) - 1)):
-            read_file(files[int(selection)])
-        else:
-            print('Invalid selection')
-    elif (selection == 'L'):
+    print("\n")
+    selection = input("Enter file no. to select file\nEnter L to change directories\nEnter N to create a new file\nEnter Q to quit\n")
+    print(selection)
+    int_bool = False
+    try:
+        fileno = int(selection)
+    except:
+        int_bool = False
+    else:
+        int_bool = True
+    if (selection == 'L'):
         print('Chose L')
     elif (selection == 'N'):
         create_file()
     elif (selection == 'Q'):
         print('Goodbye')
+    elif (int_bool):
+        if (int(selection) >= 0 and int(selection) <= len(files)):
+            read_file(files[int(selection)])
+        else:
+            print('Invalid input. Select a valid file')
+            main()
     else:
         print('Invalid input')
         main()
